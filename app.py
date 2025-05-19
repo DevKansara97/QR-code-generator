b4ffc2dd7a4a31b89009ea773f78871b4d7e7306
DevKansara97 <154012358+DevKansara97@users.noreply.github.com>
2025-05-19 13:27:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, send_file, render_template
+import qrcode
+import os
+from datetime import datetime
+from werkzeug.utils import secure_filename
+from urllib.parse import quote
+
+app = Flask(__name__)
+
+if not os.path.exists("QR Code"):
+    os.makedirs("QR Code")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_qr_code():
+    data = request.json
+    title = secure_filename(data.get('title', 'default_title'))
+    link = data.get('link', '')
+    location = data.get('location', '')
+
+    if not link and not location:
+        return jsonify({"error": "Please provide either a link or a location"}), 400
+
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    filename = f"{title}_{timestamp}.png"
+    filepath = os.path.join("QR Code", filename)
+
+    if location:
+        qr_data = f"https://www.google.com/maps/dir//{quote(location)}"
+    else:
+        qr_data = link
+
+    img = qrcode.make(qr_data)
+    img.save(filepath)
+
+    return jsonify({"message": "QR Code generated successfully", "filename": filepath})
+
+@app.route('/download/<path:filename>', methods=['GET'])
+def download_qr_code(filename):
+    filepath = os.path.join(os.getcwd(), filename)
+    if os.path.exists(filepath):
+        return send_file(filepath, as_attachment=True)
+    return jsonify({"error": "File not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
